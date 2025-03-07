63f183210fe989b0d54108bd889a03e9867f8aac
NikolaCerna <nikolacerna02@gmail.com>
2025-03-07 16:37:57 +0100

    pridanie otazok a odpovedi

---
+++ otazky.php
@@ -0,0 +1,16 @@
+<?php
+$otazky = array(
+    "Aké sú vaše skúsenosti s PHP?",
+    "Aký je váš obľúbený programovací jazyk?",
+    "Aké sú vaše ciele pre túto stránku?",
+    "Aké je najlepšie zviera?"
+);
+
+$odpovede = array(
+    "Mám niekoľkoročné skúsenosti s PHP, pracoval(a) som na rôznych projektoch, ako sú webové aplikácie a e-commerce stránky. Používam PHP na serverovej strane a pracujem so frameworkmi ako Laravel a Symfony.",
+    "Môj obľúbený programovací jazyk je Python, pretože je veľmi flexibilný a jeho syntax je čitateľná. Rád(a) používam Python na analýzu dát, machine learning a web development.",
+    "Cieľom tejto stránky je poskytovať informácie a zdroje pre ľudí, ktorí sa zaujímajú o technológie a programovanie, a vytvárať komunitu, ktorá sa navzájom podporuje pri učení.",
+    "Najlepšie zviera je ťažké určiť, pretože každé zviera má svoje špeciálne vlastnosti. Osobne mám rád(a) psy, pretože sú verní, priateľskí a skvelí spoločníci."
+);
+
+?>
