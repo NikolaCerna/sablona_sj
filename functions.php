7f79509dad2357c895497d5c334f326074865370
NikolaCerna <nikolacerna02@gmail.com>
2025-03-18 17:07:01 +0100

    zmena funkcie + jsonu
    fukncia na pridanie nadpisu pomocou jsonu a preklik obrazkom na inu stranku (obrazok alebo moze byt zmenou url aj ine)

--- functions.php
+++ functions.php
@@ -1,20 +1,22 @@
 <?php
-
-
 function generateSlides($dir) {
     $files = glob($dir . "/*.jpg");
     $json = file_get_contents("data/datas.json");
     $data = json_decode($json, true);
     $text = $data["banner_text"];
-    print_r($files);
-    print_r( realpath("img/banners/"));
+
     foreach ($files as $file) {
-        echo '<div class="slide fade">';
-        echo '<img src="' . $file . '">';
-        echo '<div class="slide-text">';
-        echo ($text[basename($file)]);
-        echo '</div>';
-        echo '</div>';
+        $filename = basename($file);
+        if (isset($text[$filename])) {
+            echo '<div class="slide fade">';
+            echo '<a href="' . ($text[$filename]["url"]) . '" target="_blank">';
+            echo '<img src="' . $file . '">';
+            echo '</a>';
+            echo '<div class="slide-text">';
+            echo ($text[$filename]["nadpis"]);
+            echo '</div>';
+            echo '</div>';
+        }
     }
 }
 ?>
